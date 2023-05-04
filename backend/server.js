99c25d4349ff3c818a70843b29b3ffcddcb1fb26
harshit <harshit.sahu@gmail.com>
2023-05-05 01:38:59 +0530

    google Outh and signup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { connection } = require('./configs/connection');
 const { userRoute } = require('./route/userRoute.route');
 const { teacherRoute } = require('./route/teacherRoute.route');
+const {googleRouter}  = require("./route/googleAuth.route")
+
 
 require('dotenv').config();
 
@@ -10,6 +12,8 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+const cors = require('cors');
+app.use(cors());
 
 app.get('/',(req,res)=>{
     res.send('Welcome to SkillSphere!');
@@ -18,6 +22,7 @@ app.get('/',(req,res)=>{
 
 app.use('/users',userRoute);
 app.use('/teachers',teacherRoute);
+app.use("/",googleRouter)
 
 app.listen(process.env.port,async()=>{
     try{
