0eb0427e45d49c69c3daf0106a34fa36041451e7
Md Ashik Ansari <aashiqmohd04@gmail.com>
2023-05-06 23:31:12 +0530

    ashik's 4th day second commit, worked on slot model

--- backend/model/slot.model.js
+++ backend/model/slot.model.js
@@ -1,23 +1,13 @@
 const mongoose = require("mongoose");
 
-const slotOnAParticularDateSchema = mongoose.Schema({
-  startTime: { type: String, required: true },
-  endTime: { type: String, required: true },
-  day: { type: String, required: true },
-  date: { type: Number, required: true },
-  month: { type: String, required: true },
-  year: { type: Number, required: true },
-  isBooked: { type: Boolean, default: false },
-});
-
 const slotSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
   },
-  slotOnAParticularDate: {
-    type: [slotOnAParticularDateSchema],
-  },
+  dateMonthName: { type: String },
+  slot_timing: [String],
+  isBooked: { type: Boolean, default: false },
 });
 
 const SlotModel = mongoose.model("slots", slotSchema);
