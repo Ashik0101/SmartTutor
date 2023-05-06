ecedffa41c665df521a13329800e5f5be9de67a4
Kabilan <112754513+kapilmascul@users.noreply.github.com>
2023-05-06 23:47:37 +0530

    Update tutor.js

--- frontend/tutor/tutor.js
+++ frontend/tutor/tutor.js
@@ -1,5 +1,88 @@
-let userName = document.getElementById('userName')
-userName.innerText = localStorage.getItem('user-name');
+let tutors = document.getElementById("tutors");
+// Fetching the data from the Tutor-Route
+const url = 'http://localhost:9090/teachers/one'
+
+fetch("http://localhost:9090/teachers/all")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    TutorsDomain(data);
+  });
+
+function TutorsDomain(data) {
+  tutors.innerHTML = `${data
+    .map((element, index) =>
+      tutorCard(
+        element._id,
+        element.name,
+        element.designation,
+        element.subjects,
+        element.description,
+        element.address,
+        element.workingHrs,
+        element.experience,
+        element.teachingExp
+      )
+    )
+    .join(" ")}`;
+}
+
+function tutorCard(
+  _id,
+  name,
+  designation,
+  subjects,
+  description,
+  address,
+  workingHrs,
+  experience,
+  teachingExp
+) {
+  return `<a href= ${url}/${_id}>
+  <div class="tutor-box">
+                    <div class="tutorIndividual">
+                        <div class="name">
+                            <h2>${name}</h2>
+                            <h4>${designation}</h4>
+                        </div>
+                        <div class="sub">
+                            <h5>${subjects[0].name}</h5>
+                            <h5>${subjects[1].name}</h5>
+                        </div>
+                        <div class="desc">
+                            <p>
+                                ${description}
+                            </p>
+                        </div>
+                        <div class="extraDetail">
+                            <div class="logos">
+                            <i class="uil uil-map-marker"></i>
+                            <h6>${address}</h6>
+                            </div>
+                            <div class="logos">
+                            <i class="uil uil-hourglass"></i>
+                            <h6>${workingHrs} Hr</h6>
+                            </div>
+                            <div class="logos">
+                            <i class="uil uil-clipboard"></i>
+                            <h6>${experience} yrs</h6>
+                            </div>
+                            <div class="logos">
+                            <i class="uil uil-presentation-edit"></i>
+                            <h6>${teachingExp} yrs</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+  </a>
+    `;
+}
+
+let userName = document.getElementById("userName");
+userName.innerText = localStorage.getItem("user-name");
+
 
 ///filtering by state & subjects
 
@@ -7,7 +90,6 @@ let searchBtn = document.getElementById("searchBtn");
 let subjectInput = document.getElementById("subjectInput");
 let locationInput = document.getElementById("locationInput");
 let filteredData=[]
-
 
 searchBtn.addEventListener("click",()=>{
   filteredData=[];
